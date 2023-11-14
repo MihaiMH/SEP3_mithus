8d14a7de00b43cc5dd67f990dbd7b3587c0994a7
Josan03 <josan.c2003@gmail.com>
2023-11-14 13:28:16 +0100

    Change the method delete post to set the status of the post to hidden.

--- DataTier/mithus/src/main/java/dk/via/mithus/DAOImpl/PostDAOImpl.java
+++ DataTier/mithus/src/main/java/dk/via/mithus/DAOImpl/PostDAOImpl.java
@@ -3,6 +3,7 @@ package dk.via.mithus.DAOImpl;
 import dk.via.mithus.DAOInterfaces.PostDAO;
 import dk.via.mithus.Shared.Post;
 import dk.via.mithus.Shared.PostStatus;
+import dk.via.mithus.Shared.PostStatuses;
 import dk.via.mithus.repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -38,7 +39,7 @@ public class PostDAOImpl implements PostDAO {
     public void deletePost(Long postId) {
         Optional<Post> post = postRepository.findById(postId);
         if (post.isPresent()) {
-            post.get().setStatus(new PostStatus("DELETED"));
+            post.get().setStatus(PostStatuses.Hidden);
             postRepository.save(post.get());
         }
     }

203cde4f9442d77390effd08a9803f53c3aa8f11
igorcretu <64871246+igorcretu@users.noreply.github.com>
2023-11-22 17:36:15 +0100

    PostHttpClient + IPostService

--- PresentationTier/HttpClients/ClientImplementations/PostHttpClient.cs
+++ PresentationTier/HttpClients/ClientImplementations/PostHttpClient.cs
@@ -1,4 +1,5 @@
 using System.Net.Http.Json;
+using System.Text;
 using System.Text.Json;
 using Domain.DTOs;
 using Domain.Models;
@@ -15,7 +16,7 @@ public class PostHttpClient : IPostService
         this.httpClient = httpClient;
     }
 
-    public async Task<Post> CreatePostAsync(PostCreationDTO dto)
+    public async Task<Post> CreateCommentAsync(PostCreationDTO dto)
     {
         HttpResponseMessage responseMessage = await httpClient.PostAsJsonAsync("/Post/createPost", dto);
         
@@ -32,11 +33,7 @@ public class PostHttpClient : IPostService
 
         return post;
     }
-
-    public async Task<Post> GetByIdAsync(int id)
-    {
-        throw new NotImplementedException();
-    }
+    
 
     public async Task<IEnumerable<Post>> GetAllPostsAsync()
     {
@@ -52,5 +49,119 @@ public class PostHttpClient : IPostService
             PropertyNameCaseInsensitive = true
         })!;
         return users;
+    }
+
+    public async Task DeletePostAsync(long id)
+    {
+        HttpResponseMessage responseMessage = await httpClient.DeleteAsync($"/Post/deletePost?id={id}");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            string content = await responseMessage.Content.ReadAsStringAsync();
+            throw new Exception(content);
+        }
+    }
+
+    public async Task SetPostStatusAsync(long postId, long statusId)
+    {   StringContent query = new StringContent(ConstructQuery(postId, statusId));
+        HttpResponseMessage responseMessage =
+            await httpClient.PatchAsync("/Post/setPostStatus", query);
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            string content = await responseMessage.Content.ReadAsStringAsync();
+            throw new Exception(content);
+        }
+    }
+
+    public async Task UpdatePostAsync(PostCreationDTO dto)
+    {
+        string dtoAsJson = JsonSerializer.Serialize(dto);
+        StringContent body = new StringContent(dtoAsJson, Encoding.UTF8, "application/json");
+        
+        HttpResponseMessage responseMessage =
+            await httpClient.PutAsync("/Post/setPostStatus", body);
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            string content = await responseMessage.Content.ReadAsStringAsync();
+            throw new Exception(content);
+        }
+    }
+
+    public async Task<Post> GetPostByPostIdAsync(long id)
+    {
+        HttpResponseMessage response = await httpClient.GetAsync($"/todos/{id}");
+        string content = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(content);
+        }
+        Post users = JsonSerializer.Deserialize<Post>(content, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return users;
+    }
+
+    public async Task<IEnumerable<PostStatus>> GetPostStatusesAsync()
+    {
+        string uri = "/Post/GetPostStatuses";
+        HttpResponseMessage response = await httpClient.GetAsync(uri);
+        string result = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(result);
+        }
+        IEnumerable<PostStatus> users = JsonSerializer.Deserialize<IEnumerable<PostStatus>>(result, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return users;
+    }
+
+    public async Task<IEnumerable<EnergyRating>> GetEnergyRatingsAsync()
+    {
+        string uri = "/Post/GetEnergyRatings";
+        HttpResponseMessage response = await httpClient.GetAsync(uri);
+        string result = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(result);
+        }
+        IEnumerable<EnergyRating> users = JsonSerializer.Deserialize<IEnumerable<EnergyRating>>(result, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return users;
+    }
+
+    public async Task<IEnumerable<HousingType>> GetHousingTypesAsync()
+    {
+        string uri = "/Post/GetHousingTypes";
+        HttpResponseMessage response = await httpClient.GetAsync(uri);
+        string result = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(result);
+        }
+        IEnumerable<HousingType> users = JsonSerializer.Deserialize<IEnumerable<HousingType>>(result, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return users;
+    }
+    private static string ConstructQuery(long postId, long statusId)
+    {
+        string query = "";
+        if (postId != null)
+        {
+            query += $"?postId={postId}";
+        }
+
+        if (statusId != null)
+        {
+            query += string.IsNullOrEmpty(query) ? "?" : "&";
+            query += $"statusId={statusId}";
+        }
+
+        return query;
     }
 }
--- PresentationTier/HttpClients/ClientInterfaces/IPostService.cs
+++ PresentationTier/HttpClients/ClientInterfaces/IPostService.cs
@@ -5,8 +5,13 @@ namespace HttpClients.ClientInterfaces;
 
 public interface IPostService
 {
-    Task<Post> CreatePostAsync(PostCreationDTO dto);
-    Task<Post> GetByIdAsync(int id);
     Task<IEnumerable<Post>> GetAllPostsAsync();
-    // Task<IEnumerable<Post>> GetAsync(SearchPostParametersDto dto);
+    Task<Post> CreateCommentAsync(PostCreationDTO dto);
+    Task DeletePostAsync(long id);
+    Task SetPostStatusAsync(long postId, long statusId);
+    Task UpdatePostAsync(PostCreationDTO dto);
+    Task<Post> GetPostByPostIdAsync(long id);
+    Task<IEnumerable<PostStatus>> GetPostStatusesAsync();
+    Task<IEnumerable<EnergyRating>> GetEnergyRatingsAsync();
+    Task<IEnumerable<HousingType>> GetHousingTypesAsync();
 }

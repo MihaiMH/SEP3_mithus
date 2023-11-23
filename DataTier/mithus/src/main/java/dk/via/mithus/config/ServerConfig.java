ec3cad74a462c3de546b203f06021bebbda60d88
Mihai Mihaila <crut173@gmail.com>
2023-11-23 14:12:11 +0100

    upd

--- DataTier/mithus/src/main/java/dk/via/mithus/config/ServerConfig.java
+++ DataTier/mithus/src/main/java/dk/via/mithus/config/ServerConfig.java
@@ -1,27 +1,44 @@
-//package dk.via.mithus.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import services.AddressService;
-//import services.AmenityService;
-//import services.CostService;
-//import services.PostService;
-//
-//@Configuration
-//public class ServerConfig {
-//    @Bean public AddressService getAddressService() {
-//        return new AddressService();
-//    }
-//
-//    @Bean public AmenityService getAmenityService() {
-//        return new AmenityService();
-//    }
-//
-//    @Bean public CostService getCostService() {
-//        return new CostService();
-//    }
-//
-//    @Bean public PostService getPostService() {
-//        return new PostService();
-//    }
-//}
+package dk.via.mithus.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import services.*;
+
+@Configuration
+public class ServerConfig {
+    @Bean public AddressService getAddressService() {
+        return new AddressService();
+    }
+
+    @Bean public AmenityService getAmenityService() {
+        return new AmenityService();
+    }
+
+    @Bean public CostService getCostService() {
+        return new CostService();
+    }
+
+    @Bean public PostService getPostService() {
+        return new PostService();
+    }
+
+    @Bean public ChatService getChatService() {
+      return new ChatService();
+    }
+
+    @Bean public EnergyRatingService getEnergyRatingService() {
+      return new EnergyRatingService();
+    }
+
+    @Bean public HousingTypeService getHousingTypeService() {
+      return new HousingTypeService();
+    }
+
+    @Bean public PostStatusService getPostStatusService() {
+      return new PostStatusService();
+    }
+
+    @Bean public UserService getUserService() {
+      return new UserService();
+    }
+}

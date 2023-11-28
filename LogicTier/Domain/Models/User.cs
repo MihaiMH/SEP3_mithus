5c036116cd154eba21ea45ba50c86f71ae6e63d6
igorcretu <64871246+igorcretu@users.noreply.github.com>
2023-11-28 22:41:11 +0100

    User Domanin model updated nullable

--- LogicTier/Domain/Models/User.cs
+++ LogicTier/Domain/Models/User.cs
@@ -4,7 +4,7 @@ public class User
 {
     public long ID { get; set; }
     public string Email { get; set; }
-    public string Password { get; set; }
+    public string? Password { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public Role Role { get; set; }
